2dfd003d649905a86c46d5906b44c227727f3c03
zettdaymond <zettday@gmail.com>
2015-01-15 03:52:10 +0500

    Start migration property analyse plugin in class;

---
+++ plugins/netproperty/propertyanalyser.h
@@ -0,0 +1,14 @@
+#ifndef PROPERTYANALYSER_H
+#define PROPERTYANALYSER_H
+
+#include <QObject>
+class PropertyAnalyser : public QObject
+{
+    Q_OBJECT
+public:
+
+    PropertyAnalyser() {}
+    virtual ~PropertyAnalyser() {}
+};
+
+#endif
